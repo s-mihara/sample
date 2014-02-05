b9c9f3ace01ff7c872171fa2b8de12bd4a9ff948
s-mihara <pub.pyros@gmail.com>
2014-02-06 02:14:10 +0900

    pull req テスト用コミット

--- helloworld_copy.js
+++ helloworld_copy.js
@@ -1,4 +1,6 @@
 // copyして、あえてもう一個作成。で更新.さらに
+// 無駄な変数を宣言してはいかがでしょうか。
+var muda;
 var http = require('http');
 http.createServer(function (req, res) {
 	res.writeHead(200,{'Content-Type':'text/plain'} );
